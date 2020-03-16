3d45a5ec7dd245556dbbeff6d997f9ac22b091a8
IvanR <yu181219710>
2020-03-17 00:09:42 +0500

    styles and mock for comment

--- src/components/CommentTemplate.vue
+++ src/components/CommentTemplate.vue
@@ -1,6 +1,30 @@
 <template>
-    <div>
-
+    <div class="comment">
+        <div class="comment-avatar">
+            <img src="../assets/empty-avatar.png" alt="avatar">
+        </div>
+        <div class="comment-body">
+            <div class="comment-body-nav">
+                <router-link class="comment-body-nav-name" to="/">Lorem ipsum.</router-link>
+                <div class="comment-body-nav-time">Час назад</div>
+                <div class="comment-body-nav-vote">
+                    <button>+</button>
+                    <button>+1</button>
+                    <button>-</button>
+                </div>
+                <div class="comment-body-nav-reply">Ответить</div>
+            </div>
+            <div class="comment-body-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea earum eius optio sapiente.
+                Aliquid asperiores deleniti ipsa repellendus sint! Adipisci amet dolor dolore dolores ea
+                exercitationem id impedit itaque iure labore nemo nesciunt nihil nulla odit possimus quod quos
+                tempora, vitae voluptates voluptatibus. Beatae deserunt ducimus nam quas sed. Adipisci cumque
+                dignissimos ducimus eaque eius enim expedita fugiat labore odio optio rem reprehenderit rerum unde
+                voluptatem, voluptatibus. Accusamus amet culpa dolor dolores id laboriosam nesciunt placeat quod
+                tempora vitae! Aliquam animi commodi deleniti dolor earum, ex illum iste laborum minus, numquam
+                quidem quo rerum suscipit totam voluptas! Atque, dignissimos eius!
+            </div>
+        </div>
     </div>
 </template>
 
