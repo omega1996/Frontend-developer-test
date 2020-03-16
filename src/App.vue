018aafb678c9e7e894cdac32014e9ef0cc4f9f04
IvanR <yu181219710>
2020-03-16 18:40:02 +0500

    Main frame for project initiated

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+<style lang="stylus" src="./styles/index.styl" rel="preload"></style>
+
+<script>
+ export default {
+  name: 'App',
+
+}
+</script>
+
---
+++ src/views/Article.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <router-link class="" to="/"> Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Cum doloremque obcaecati quisquam
+            reprehenderit soluta. Alias cum iure minima molestiae
+            nam nihil totam voluptatibus? Aspernatur nisi officiis
+            quod temporibus ut vero.</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Article"
+    }
+</script>
+
+<style scoped>
+
+</style>
