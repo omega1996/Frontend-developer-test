eb8e710808514869d9c016c292817b50bd9ebbf3
IvanR <yu181219710>
2020-03-18 17:08:34 +0500

    did right positions for comments getter

--- src/store/modules/comment.js
+++ src/store/modules/comment.js
@@ -8,7 +8,7 @@ export default {
                 id: 0,
                 reply: 0,
                 rating: 0,
-                text: 'lorem',
+                text: 'lorem first',
                 userName: 'Lorem Ipsum',
                 timeStamp: 1584420823049
             },
@@ -16,7 +16,7 @@ export default {
                 id: 1,
                 reply: 0,
                 rating: 1,
-                text: 'lorem Ipsum',
+                text: 'lorem Ipsum second reply to first',
                 userName: 'Lorem Ipsum',
                 timeStamp: 1584420823049
             },
@@ -24,7 +24,7 @@ export default {
                 id: 2,
                 reply: 1,
                 rating: 0,
-                text: 'lorem',
+                text: 'lorem me reply to second',
                 userName: 'Lorem Ipsum',
                 timeStamp: 1584420823049
             },
@@ -36,6 +36,39 @@ export default {
                 userName: 'Lorem Ipsum',
                 timeStamp: 1584420823049
             },
+            {
+                id: 5,
+                reply: 0,
+                rating: 2,
+                text: 'lorem ipsum dolor sit amet reply to first',
+                userName: 'Lorem Ipsum',
+                timeStamp: 1584420823049
+            },
+            {
+                id: 6,
+                reply: 2,
+                rating: 2,
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, ' +
+                    'blanditiis deleniti dignissimos doloribus eveniet fuga fugiat iure quibusdam quos velit.',
+                userName: 'Lorem Ipsum',
+                timeStamp: 1584420823049
+            },
+            {
+                id: 7,
+                reply: 6,
+                rating: 2,
+                text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, ',
+                userName: 'Lorem Ipsum',
+                timeStamp: 1584420823049
+            },
+            {
+                id: 8,
+                reply: 5,
+                rating: 2,
+                text: ' reply to dolor first',
+                userName: 'Lorem Ipsum',
+                timeStamp: 1584420823049
+            },
         ]
     },
     getters: {
@@ -43,19 +76,25 @@ export default {
             return state.comments
         },
         allMockComments(state){
-            return state.mockComments.map((element, index, array) => {
+            let answer =[];
+            state.mockComments.map((element) => {
                 if (element.reply === element.id) {
-                    element.level = 0
+                    element.level = 0;
+                    answer.push(element)
                 } else {
-                    let parent = array.find(value => value.id === element.reply);
-                    if (parent.reply === parent.id){
+                    let parent = answer.find(value => value.id === element.reply);
+                    let parentIndex = answer.findIndex(value => value.id === element.reply);
+                    if (parent.reply === parent.id) {
                         element.level = 1
                     } else {
                         element.level = 2
                     }
+                    answer.splice(parentIndex,0,element)
+
                 }
                 return element
-            })
+            });
+            return answer.reverse()
         }
     }
 }
