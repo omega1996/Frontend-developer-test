fcf8f3c8740646573a2a96fc04807c250b89b697
IvanR <yu181219710>
2020-03-18 17:40:30 +0500

    added button showing comment

--- src/components/CommentTemplate.vue
+++ src/components/CommentTemplate.vue
@@ -1,21 +1,24 @@
 <template>
-    <div class="comment" :class="'comment-level-'+level">
-        <div class="comment-avatar">
-            <img src="../assets/empty-avatar.png" alt="avatar">
-        </div>
-        <div class="comment-body">
-            <div class="comment-body-nav">
-                <router-link class="comment-body-nav-name" to="/">{{ authorName }}</router-link>
-                <div class="comment-body-nav-time">Час назад</div>
-                <div class="comment-body-nav-vote">
-                    <button>+</button>
-                    <button>{{ rating }}</button>
-                    <button>-</button>
-                </div>
-                <div class="comment-body-nav-reply">Ответить</div>
+    <div>
+        <button :class="'comment-level-'+level" v-if="badKarma">Показать комментарий</button>
+        <div class="comment" :class="'comment-level-'+level" v-else>
+            <div class="comment-avatar">
+                <img src="../assets/empty-avatar.png" alt="avatar">
             </div>
-            <div class="comment-body-text">
-                <slot></slot>
+            <div class="comment-body">
+                <div class="comment-body-nav">
+                    <router-link class="comment-body-nav-name" to="/">{{ authorName }}</router-link>
+                    <div class="comment-body-nav-time">Час назад</div>
+                    <div class="comment-body-nav-vote">
+                        <button>+</button>
+                        <button>{{ rating }}</button>
+                        <button>-</button>
+                    </div>
+                    <div class="comment-body-nav-reply">Ответить</div>
+                </div>
+                <div class="comment-body-text">
+                    <slot></slot>
+                </div>
             </div>
         </div>
     </div>
@@ -24,7 +27,12 @@
 <script>
 export default {
     name: "CommentTemplate",
-    props:{
+    computed: {
+        badKarma() {
+            return this.rating < -10
+        }
+    },
+    props: {
         level: {
             type: Number,
             default: 0
