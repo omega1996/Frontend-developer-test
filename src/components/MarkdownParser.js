0301ea0c4006bcfc9b1a35f2d28080337630d02d
IvanR <yu181219710>
2020-03-20 00:59:15 +0500

    links and images md syntax

--- src/components/MarkdownParser.js
+++ src/components/MarkdownParser.js
@@ -23,6 +23,31 @@ function parse(value) {
         });
     }
 
+    // checking for links and images
+
+    let processMatched = function (matched, typeTag) {
+        let indexOfLink = matched.lastIndexOf('(');
+        let link = matched.slice(indexOfLink + 1, matched.length - 1);
+        let text;
+        if (matched.includes('[')) {
+            text = matched.slice(1, indexOfLink - 1);
+        } else {
+            text = link;
+        }
+        if (typeTag === 'img'){
+            return `<img src="${link}" alt="${text}">`
+        } else if (typeTag === 'a'){
+            return `<a href="${link}">` + text + '</a>'
+        }
+
+    };
+
+    let imgRe = /(![[](.*?)[\]])\(.*?\)/;
+    value = value.replace(imgRe, (matched) => processMatched(matched,'img'));
+
+    let linkRe = /([[](.*?)[\]])?\(.*?\)/gi;
+    value = value.replace(linkRe, (matched) => processMatched(matched,'a'));
+
     return value
 }
 
