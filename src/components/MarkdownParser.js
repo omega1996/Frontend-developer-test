6191a1cf948e1bcc10bcb20dd7bd7d09b7763838
IvanR <yu181219710>
2020-03-21 23:30:33 +0500

    refactored md parser, left regexps declaration outside main function

--- src/components/MarkdownParser.js
+++ src/components/MarkdownParser.js
@@ -1,46 +1,68 @@
-function parse(value) {
-    // italic, bold and bold-italic text
-    let regExps = [
-        {
-            re: /\*{3}(.*?)\*{3}/gi,
-            tagBefore: '<b><i>',
-            tagAfter: '</i></b>'
-        },
-        {
-            re: /\*{2}(.*?)\*{2}/gi,
-            tagBefore: '<b>',
-            tagAfter: '</b>'
-        },
-        {
-            re: /\*(.*?)\*/gi,
-            tagBefore: '<i>',
-            tagAfter: '</i>'
-        },
-        {
-            re: /_(.*?)_/gi,
-            tagBefore: '<i>',
-            tagAfter: '</i>'
-        },
-        {
-            re: /~(.*?)~/gi,
-            tagBefore: '<s>',
-            tagAfter: '</s>'
-        },
-        {
-            re: /`(.*?)`/gi,
-            tagBefore: '<span style="background: lightgray">',
-            tagAfter: '</span>'
-        }
-    ];
-    // headers
-    for (let i = 6; i > 0; i--) {
-        let obj = {
-            re: new RegExp(`^#{${i}}(.*)#*`, 'gmiu'),
-            tagBefore: `<h${i}>`,
-            tagAfter: `</h${i}>`
-        };
-        regExps.push(obj)
+// italic, bold and bold-italic, monospace, deleted text
+let regExps = [
+    {
+        re: /\*{3}(.*?)\*{3}/gi,
+        tagBefore: '<b><i>',
+        tagAfter: '</i></b>'
+    },
+    {
+        re: /\*{2}(.*?)\*{2}/gi,
+        tagBefore: '<b>',
+        tagAfter: '</b>'
+    },
+    {
+        re: /\*(.*?)\*/gi,
+        tagBefore: '<i>',
+        tagAfter: '</i>'
+    },
+    {
+        re: /_(.*?)_/gi,
+        tagBefore: '<i>',
+        tagAfter: '</i>'
+    },
+    {
+        re: /~(.*?)~/gi,
+        tagBefore: '<s>',
+        tagAfter: '</s>'
+    },
+    {
+        re: /`(.*?)`/gi,
+        tagBefore: '<span style="background: lightgray">',
+        tagAfter: '</span>'
     }
+];
+// headers
+for (let i = 6; i > 0; i--) {
+    let obj = {
+        re: new RegExp(`^#{${i}}(.*)#*`, 'gmiu'),
+        tagBefore: `<h${i}>`,
+        tagAfter: `</h${i}>`
+    };
+    regExps.push(obj)
+}
+
+// checking for links and images
+let imgRe = /(![[](.*?)[\]])\(.*?\)/gi;
+let linkRe = /([[](.*?)[\]])?\(.*?\)/gi;
+
+let processMatched = function (matched, typeTag) {
+    let indexOfLink = matched.lastIndexOf('(');
+    let link = matched.slice(indexOfLink + 1, matched.length - 1);
+    let text;
+    if (matched.includes('[')) {
+        text = matched.slice(1, indexOfLink - 1);
+    } else {
+        text = link;
+    }
+    if (typeTag === 'img') {
+        return `<img src="${link}" alt="${text}">`
+    } else if (typeTag === 'a') {
+        return `<a href="${link}">` + text + '</a>'
+    }
+};
+
+
+function parse(value) {
 
     for (let regExp of regExps) {
         value = value.replace(regExp.re, (matched, inside) => {
@@ -54,28 +76,8 @@ function parse(value) {
         return '<hr>'
     });
 
-    // checking for links and images
-
-    let processMatched = function (matched, typeTag) {
-        let indexOfLink = matched.lastIndexOf('(');
-        let link = matched.slice(indexOfLink + 1, matched.length - 1);
-        let text;
-        if (matched.includes('[')) {
-            text = matched.slice(1, indexOfLink - 1);
-        } else {
-            text = link;
-        }
-        if (typeTag === 'img') {
-            return `<img src="${link}" alt="${text}">`
-        } else if (typeTag === 'a') {
-            return `<a href="${link}">` + text + '</a>'
-        }
-    };
-
-    let imgRe = /(![[](.*?)[\]])\(.*?\)/gi;
     value = value.replace(imgRe, (matched) => processMatched(matched, 'img'));
 
-    let linkRe = /([[](.*?)[\]])?\(.*?\)/gi;
     value = value.replace(linkRe, (matched) => processMatched(matched, 'a'));
 
     // line breaks
